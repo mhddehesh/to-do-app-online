89e0daff5126d6feb808a4b9e5f4d2a3f6315453
mhddehesh <mhd.dehesh@gmail.com>
2024-04-07 18:15:16 +0200

    Initial commit

--- Home.py
+++ Home.py
@@ -5,10 +5,13 @@ def add_todo():
     todo=st.session_state['new_todo']+'\n'
     todos.append(todo)
     functions.write_todos(todos)
-
+#st.set_page_config(layout='wide')
 st.title('This is a to Do-App')
 st.subheader('We use this web-app to manage our daily todos')
-st.write("Let's try it:")
+st.write("Let's try <b>it:</b>")
+
+st.text_input(label='Enter to do below:',placeholder='Write a To_Do.....'
+              , on_change=add_todo,key='new_todo')
 
 for index,todo in enumerate(todos):
     checkbox=st.checkbox(todo,key=todo)
@@ -19,5 +22,4 @@ for index,todo in enumerate(todos):
         st.experimental_rerun()
 
 
-st.text_input(label='Enter to do below:',placeholder='Write a To_Do.....'
-              , on_change=add_todo,key='new_todo')
+
---
+++ pages/about.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.write('This app is writen by Mohammad, and is  his 1st program on cloud.'
+         'more over, there is a clue that Man karo hamejure dar miaram ;)')
